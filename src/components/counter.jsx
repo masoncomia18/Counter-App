2cbbe6437906be33dbd44d6265006b1fdbdb0749
Mason Comia <mason.comia18@yahoo.com>
2020-06-10 19:23:58 -0700

    App functionality completed Added Navbar Added Navbar Display

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,27 +1,18 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    value: this.props.value,
-  };
-
-  handleIncrement = (product) => {
-    this.setState({ value: this.state.value + 1 });
-  };
-
   render() {
-    console.log(this.props);
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={(product) => this.handleIncrement(product)}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btn-sm"
         >
           Increment
         </button>
         <button
-          onClick={this.props.onDelete}
+          onClick={() => this.props.onDelete(this.props.counter.id)}
           className="btn btn-danger sml m-2"
         >
           Delete
@@ -31,13 +22,13 @@ class Counter extends Component {
   }
 
   formatCount() {
-    const { value } = this.state;
+    const { value } = this.props.counter;
     return value === 0 ? "Zero" : value;
   }
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.value === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 }
